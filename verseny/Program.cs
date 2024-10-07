2362f9233ae7d42cc165082e7d7d46064d62492c
Bástyai Botond <64700017+botator@users.noreply.github.com>
2024-10-07 10:39:50 +0200

    Add files via upload

---
+++ verseny/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using verseny;
+
+List<Versenyzők> lista = [];
+using StreamReader sr = new(path: "C:\\Users\\bastyai.botond\\Desktop\\verseny\\verseny\\src\\forras.txt",
+    encoding: Encoding.UTF8);
+while (!sr.EndOfStream)
+{
+    lista.Add(new(sr.ReadLine()));
+}
+Console.WriteLine($"ennyi van {lista[1].ToString()}");
+
+Console.WriteLine("elit versenyzők");
+var linqelit = lista.Where(p => p.category == "elit");
+Console.WriteLine($"{linqelit.Count()}");
+
+var linqnoelet = lista.Where(p => p.sex =="n").Average(d =>DateTime.Now.Year - d.age)+" év";
+Console.WriteLine("nok atlag eletkora " + linqnoelet);
+Console.WriteLine($"osszes id bicikli órában {Math.Round(TimeSpan.FromSeconds(lista.Sum(p =>p.biketime.TotalSeconds)).TotalHours,2)}");
+Console.WriteLine($"átlagos elit junior úszás idő {Math.Round(TimeSpan.FromSeconds(lista.Average(p => p.swimtime.TotalSeconds)).TotalHours, 2)}");
+var linquszasatlag = TimeSpan.FromSeconds(lista.Average(d => d.swimtime.TotalSeconds)).TotalHours;
+Console.WriteLine($" elit junior atlag "+linquszasatlag);
+var gyoztes = 0;
+for (int i = 0; i < lista.Count; i++)
+{
+    if(lista[i].sex == "f")
+    {
+        gyoztes += Convert.ToInt16(lista[i].swimtime.TotalSeconds);
+        gyoztes += Convert.ToInt16(lista[i].runtime.TotalSeconds);
+        gyoztes += Convert.ToInt16(lista[i].biketime.TotalSeconds);
+        gyoztes += Convert.ToInt16(lista[i].firstrest.TotalSeconds);
+        gyoztes += Convert.ToInt16(lista[i].secondrest.TotalSeconds);
+
+    }
+}
+Console.WriteLine(gyoztes);
---
+++ verseny/Versenyzők.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verseny
+{
+    internal class Versenyzők
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int rnum { get; set; }
+        public string sex { get; set; }
+        public string category  { get; set; }
+        public TimeSpan swimtime { get; set; }
+        public TimeSpan firstrest { get; set; }
+        public TimeSpan biketime { get; set; }
+        public TimeSpan secondrest { get; set; }
+        public TimeSpan runtime { get; set; }
+
+        public Versenyzők( string n) {
+            var v = n.Split(';');
+            name = v[0];
+            age = int.Parse(v[1]);
+            rnum = int.Parse(v[2]);
+            sex = v[3];
+            category = v[4];
+            swimtime = TimeSpan.Parse(v[5]);
+            firstrest = TimeSpan.Parse(v[6]);
+            biketime = TimeSpan.Parse(v[7]);
+            secondrest = TimeSpan.Parse(v[8]);
+            runtime = TimeSpan.Parse(v[9]);
+        }
+        public override string ToString()
+        {
+            var ido = DateTime.Now.Year;
+            return $"{name} {ido - age} ";
+        }
+
+
+    }
+}
